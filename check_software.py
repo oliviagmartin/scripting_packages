0460bbfc8be230af8186705461ac49780adc428a
oliviagmartin <95377239+oliviagmartin@users.noreply.github.com>
2022-08-08 18:34:02 -0700

    Create check_software.py

---
+++ check_software.py
@@ -0,0 +1,26 @@
+import time
+from playsound import playsound
+
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+print('10')
+time.sleep(1.5)
+
+playsound('audio.mp3')
+
